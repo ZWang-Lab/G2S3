17c4e759e2374a2dccfa21a4de572731501d3b53
Weimiao Wu <wwm.ccb@gmail.com>
2020-09-01 10:30:50 -0400

    Update example_script.R
    fix typo

--- example_script.R
+++ example_script.R
@@ -4,4 +4,4 @@ g2s3.dir ='.'
 source('g2s3_config.R')
 g2s3_config(unlocbox.dir, gspbox.dir, g2s3.dir) # this will configure the run_G2S3.m file for imputation
 dat = readRDS('example1.rds')
-dat_impute = run_g2s3(df)
+dat_impute = run_g2s3(dat)
